bacfc612d72b43730c4ddb63f352ba24e9ac49ea
Karthik Raj <raj@channelassist.com>
2023-01-04 15:32:02 +0530

    Latest code deployed

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 var cors = require('cors')
+var bodyParser = require('body-parser')
+
 
 
 
@@ -16,16 +18,31 @@ var cors = require('cors')
 //     console.log(error);
 //   });
 app.use(cors())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 //////
 app.get('/', async(req, res) => {
     try{
-        const date = req.query.date;
-        const token = req.query.token;
+        res.send("Hello world am working")
+    }catch(err){
+        console.log("Error Happend",err);
+        res.status(500).send("Someting happend")
+    }
+  })
+
+  app.post('/getCandles', async(req, res) => {
+    try{
+        const date = req.body.date;
+        const token = req.body.token;
         if(!token) return res.status(400).send('Token is missing')
         const url = `https://kite.zerodha.com/oms/instruments/historical/8972290/5minute?user_id=WB5864&oi=1&from=${date}&to=${date}`
         const config = {
             headers:{
-                authorization: 'enctoken dderKfuUlexIcFrXiMGcFxkuV6x81mJJDPb8/t18HJaf5xqttYHvJtsJjiwicqgAfEZ0hggaJe8bbDvkZH6m6UQ/FU+NFfsf7AFCHZ/3yBES2CeaLrTMzw==',
+                authorization: token,
               
             }
           };
@@ -33,11 +50,11 @@ app.get('/', async(req, res) => {
         res.send(response.data)
 
     }catch(err){
-        console.log("Error Happend");
+        console.log("Error Happend",err);
         res.status(500).send("Someting happend")
     }
   })
   
   app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
   })
